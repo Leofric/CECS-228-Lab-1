afd80cc5be588c97bfc81d32e0fd52f243c6827d
Alex Berthon <alexberthon@dhcp-39-34-242.dhcpint.csulb.edu>
2016-08-31 10:32:25 -0700

    Initial Commit

---
+++ Cecs 282/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cecs 282
+//
+//  Created by Alex Berthon on 8/31/16.
+//  Copyright © 2016 Alex Berthon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
