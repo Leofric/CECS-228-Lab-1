7fc3e35a25bc1859fa8b7b9c5c8da7ea8a8c4b8a
Alex Berthon <alexberthon@dhcp-39-33-226.dhcpint.csulb.edu>
2016-10-12 12:15:56 -0700

    testing

--- Cecs 282/main.cpp
+++ Cecs 282/main.cpp
@@ -1,15 +1,39 @@
-//
-//  main.cpp
-//  Cecs 282
-//
-//  Created by Alex Berthon on 8/31/16.
-//  Copyright © 2016 Alex Berthon. All rights reserved.
-//
-
 #include <iostream>
+void bubbleSort(int array[]);
+int main()
+{
+    int original[5] = {11, 2, 13, 4, 5};
+    
+    std::cout<<"Printing original array"<<std::endl;
+    for(int i = 0; i<5; i++){
+        std::cout<<original[i]<<std::endl;
+    }
+    
+    bubbleSort(original);
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    
+    std::cout<<"Printing sorted array"<<std::endl;
+    for(int i = 0; i<5; i++){
+        std::cout<<original[i]<<std::endl;
+    }
+    
+    return 0; 
+}
+
+void bubbleSort(int array[]){
+    int temp;
+    int flag = 1;
+    int numLength = 5;
+    
+    for(int i = 1; (i<=numLength) && flag; i++){
+        flag = 0;
+        for(int j=0; j<(numLength-1); j++){
+            if(array[j+1]>array[j]){
+                temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+                flag = 1;
+            }
+        }
+    }
 }
